0a3c5b94d34077eb6ad71627df8e89ebc68c020e
Jonathan Lipps <jlipps@gmail.com>
2013-10-03 20:51:52 +0100

    finish monocle kitchen sink

--- monocle-js/05-kitchen-sink.js
+++ monocle-js/05-kitchen-sink.js
@@ -9,11 +9,22 @@ var path = require('path')
 var gratuitousFileFunction = o_O(function*() {
   yield fs.writeFile(testFile, "Hello World");
   yield sleep(0.5);
+  var evenness = '';
+  if (Date.now() % 2 === 0) {
+    evenness = 'Time was even, doing nothing! ';
+  } else {
+    evenness = 'Time was odd, need to wait a ms...';
+    yield sleep(0.001);
+  }
   yield fs.appendFile(testFile, "!");
-  yield fs.appendFile(testFile, " :-)");
+  yield fs.appendFile(testFile, " " + evenness);
+  var maxNum = 10;
+  for (var i = 0; i <= maxNum; i++) {
+    yield fs.appendFile(testFile, " " + i);
+  }
   var data = yield fs.readFile(testFile);
   data = data.toString();
-  data.should.equal("Hello World! :-)");
+  data.should.equal("Hello World! " + evenness + " 0 1 2 3 4 5 6 7 8 9 10");
   yield fs.unlink(testFile);
 });
 
